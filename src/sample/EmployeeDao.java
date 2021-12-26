c80f3eac5ecfb5e4d00e40fce1b5ce873e7b45ae
3509nkf <nagokaitofabian@gmail.com>
2021-12-27 07:12:57 +0900

    .settings

--- src/sample/EmployeeDao.java
+++ src/sample/EmployeeDao.java
@@ -1,8 +1,23 @@
 package sample;
 
+import java.sql.PreparedStatement;
+
 public class EmployeeDao {
-	public Employee  update(Employee employee) {
-		return employee;
+	
+	public void update(Employee employee) {
+		String sql = "UPDATE employees SET name=? WHERE id=?";
+	    //ステートメントオブジェクトを生成
+		PreparedStatement stmt = connection.prepareStatement(sql);
+		stmt.setString(1, employee.getName());
+		stmt.setInt(2, employee.getId());
+		
+		//ステートメントの実行
+		int num = stmt.execute();
+		
+		//データべースから切断
+		stmt.close();
+		
+		return num;
 	}
 
 }
